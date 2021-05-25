e66becc1183f40f2abf32d9818d785d66af4e406
nvizcarra <nicolasvizcarra64@gmail.com>
2021-05-25 16:41:41 -0300

    Create and insert Obtain Phrase Button

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import './App.css';
 
 import styled from '@emotion/styled';
 
+const Button = styled.button`
+  background: -webkit-linear-gradient(top left, #007d35 0%, #007d35 40%, #0f574e 100%);
+  background-size: 300px;
+  font-family: Arial, Helvetica, sans-serif;
+  color: #fff;
+  margin-top: 3rem;
+  padding: 1rem 3rem;
+  font-size: 2rem;
+  border: 2px solid black;
+`;
+
 function App() {
   return (
-    
+    <Button>
+      Obtain Phrase
+    </Button>
   );
 }
 
